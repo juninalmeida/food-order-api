adceb6b2a9a7b6920cacfbbde6672eb40554b87d
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-01 13:46:41 -0300

    feat: connect product creation to database with repository types

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
+import { knex } from "@/database/knex";
 import { z } from "zod";
+import { ProductRepository } from "@/database/types/product-repository";
 
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -19,7 +21,9 @@ class ProductsController {
 
       const { name, price } = bodySchema.parse(request.body);
 
-      return response.status(201).json({ name, price });
+      await knex<ProductRepository>("products").insert({ name, price });
+
+      return response.status(201).json();
     } catch (error) {
       next(error);
     }
---
+++ src/database/types/product-repository.d.ts
@@ -0,0 +1,9 @@
+import { productsRoutes } from "@/routes/products-routes";
+
+type ProductRepository = {
+  id: number;
+  name: string;
+  price: number;
+  created_at: number;
+  updated_at: number;
+}
