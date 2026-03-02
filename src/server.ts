78b10a9365720b7145bf3336414c32e29a17316b
juninalmeida <junioralmeidati2023@gmail.com>
2026-03-02 19:47:02 -0300

    chore: setup frontend static serving, cors, and ts compiler for client-side

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+
 import { routes } from "./routes";
 import { errorHandling } from "./middlewares/error-handling";
 
 const PORT = 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(routes);
 
 app.use(errorHandling);
